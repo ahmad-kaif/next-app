fa05e83e18f59f20f4683682052dba270eef550f
Ahmad Kaif <heyahmad611@gmail.com>
2024-12-20 14:20:03 +0530

    next-practice

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,13 @@
+// 'use client'; // to make this client component from server component
+import React from 'react'
+import AddToCart from './AddToCart'
+
+const ProductCard = () => {
+  return (
+    <div>
+        <AddToCart/>
+    </div>
+  )
+}
+
+export default ProductCard
